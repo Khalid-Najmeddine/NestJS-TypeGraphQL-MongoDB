2a204669cd4a014fa0bfb39646095fda9bc0c225
Khalid Najmeddine <knajm574@gmail.com>
2022-12-15 16:42:11 -0700

    feat: starting and setting up GraphQL Server

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Parent, ResolveField } from '@nestjs/graphql';
+import { AuthorService } from 'src/author/author.service';
+import { Author } from '../author/author.schema';
+import { Book } from './book.schema';
+import { BookService } from './book.service';
+@Resolver(() => Book)
+export class BookResolver {
 
-@Resolver()
-export class BookResolver {}
+  constructor(private bookService: BookService, private authorService: AuthorService) {}
+
+  @Query(() => [Book]) //<-- what will the query return? 
+  async books /* <-- Query Name*/ () { 
+    return this.bookService.findMany() //Resolve the query
+  }
+
+  @ResolveField(() => Author)
+  async author(@Parent() book: Book) {
+    return this.authorService.findById(book.author)
+  }
+
+
+}
