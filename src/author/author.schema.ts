6cacde1440cc9614f9eae2972953d91d24870709
Khalid Najmeddine <knajm574@gmail.com>
2022-12-15 15:44:10 -0700

    feat: build the book component (schema + service)

---
+++ src/author/author.schema.ts
@@ -0,0 +1,13 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import {Book} from "../book/book.schema"
+@ObjectType()
+export class Author {
+  @Field(() => ID) // <-- GraphQL type
+  id: string // <-- TypeScript type
+
+  @Field()
+  name: string
+
+  @Field(() => [Book])
+  books: Book[]
+}
