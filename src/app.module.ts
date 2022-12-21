499a496b691c7f8125e99ea34c61e952926f0672
Khalid Najmeddine <knajm574@gmail.com>
2022-12-21 01:02:46 -0700

    feat: GraphQL Auth setup with NestJS + TypeGraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config"
 import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo"
 import {GraphQLModule} from "@nestjs/graphql";
 import { MongooseModule} from "@nestjs/mongoose"
@@ -6,14 +7,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {AuthorModule} from "./author/author.module"
 import { BookModule } from './book/book.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot("mongodb+srv://knajm574:12345@cluster0.oetx4qb.mongodb.net/?retryWrites=true&w=majority"),
     GraphQLModule.forRoot<ApolloDriverConfig>({driver: ApolloDriver, autoSchemaFile: "schema.gql"}),
     AuthorModule, 
-    BookModule
+    BookModule, UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
