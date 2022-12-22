b01a62a6d44998a9975303331ab8b6292494c38d
Khalid Najmeddine <knajm574@gmail.com>
2022-12-21 18:11:37 -0700

    feat: typegraphql mutation registerUser,

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import {Model} from "mongoose"
-import {User, UserDocument} from "./user.schema"
+import {nanoid} from "nanoid"
+import {User, UserDocument, CreateUserInput} from "./user.schema"
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(input: CreateUserInput) {
+    const confirmToken = nanoid(32) //confirm token in this example will be 32 bits long
+    return this.userModel.create({...input, confirmToken})
+  }
 }
